b844d865e54b113f47e908a4b5a39147af306006
Iván Morett <luisivanmorett@gmail.com>
2019-02-27 21:31:20 -0600

    added comments and removed useless code

--- charts.js
+++ charts.js
@@ -1,25 +1,20 @@
-function createDomElement(position, elementName, attributes){
-    let element = document.createElement(elementName);
-    for(var key in attributes){
-        let att = document.createAttribute(key);      
-        att.value = attributes[key];
-        element.setAttributeNode(att)
-    }
-    position.appendChild(element);
-    return element;
-}
-
-
 function loadCharts(querySnapshot){
+    // Obtenemos el elemento donde pondremos las gráficas
     const chartsDiv = document.getElementById("charts");
-
+    // Usamos un contador para saber en que grafica vamos
     let chartCont = 0;
-    let charts = []
+
+    // Para cada documento
     querySnapshot.forEach(function(doc) {
+        // Creamos el contenedor de la gráfica individual
         chartsDiv.innerHTML = chartsDiv.innerHTML + `<canvas id=chart`+chartCont+`></canvas>`;
+        // Obtenemos el canvas del mismo, que es donde se dibuja
         let ctx = document.getElementById("chart"+chartCont).getContext('2d');
 
+        // Obtenemos la información individual del documento
         let data = doc.data();
+
+        // Esto es para organizar la información para la gráfica
         let chartData = {
             datasets: [{
                 data: [],
@@ -27,26 +22,32 @@ function loadCharts(querySnapshot){
             }],
             labels: [ ]
         }
+
+        // Para cada llave en la información
         for(var key in data){
+            // guardamos la llave como label
             chartData.labels.push(key)
+            // guardamos el valor que tiene la llave como data
             chartData.datasets[0].data.push(data[key])
+            // generamos aleatoriamente un conjunto de colores para la gráfica
             chartData.datasets[0].backgroundColor.push('rgb('+Math.ceil(Math.random()*255)+', '+Math.ceil(Math.random()*255)+', '+Math.ceil(Math.random()*255)+')')
         }
 
-        charts.push(
-            new Chart(ctx,{
-                type: 'pie',
-                data: chartData,
-                options: {
-                    title: {
-                      display: true,
-                      text: doc.id
-                    }
-                  }
-            })
-        ); 
+        // Creamos la gráfica
+        new Chart(ctx,{
+            type: 'pie',
+            data: chartData,
+            options: {
+                title: {
+                  display: true,
+                  // mostramos el nombre de la encruzijada
+                  text: doc.id
+                }
+              }
+        })
         chartCont++;
     });
 }
 
+// Mandamos a llamar la función read de database con el callback
 read(loadCharts);
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+// Obtenemos el valor de la decisión actual, lo obtenemos aqui para obtenerlo solo una vez en vez de dos veces
 let decisionActual = document.getElementById("nombre_decision").value;
+
+// Obtenemos el id opc1 y le creamos una funcion onclick
 document.getElementById("opc1").onclick= function(){
+    // Obtenemos el texto que esta en el id txt1
     let decisionTomada = document.getElementById("txt1").innerHTML;
+    // escribimos en la base de datos, usamos el callback para redirigir la página
     write(decisionActual, decisionTomada, function(){window.location.href = "#"})
 };
 
