1c58e7a9af0bc6035080f00d2ef80881c41a3008
Iván Morett <luisivanmorett@gmail.com>
2019-03-01 08:43:18 -0600

    corrected url independence

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // Obtenemos el valor de la decisión actual, lo obtenemos aqui para obtenerlo solo una vez en vez de dos veces
 let decisionActual = document.getElementById("nombre_decision").value;
 
+// Obtenemos los urls
+var url1 = document.getElementById("url1").value;
+var url2 = document.getElementById("url2").value;
+
 // Obtenemos el id opc1 y le creamos una funcion onclick
 document.getElementById("opc1").onclick= function(){
     // Obtenemos el texto que esta en el id txt1
     let decisionTomada = document.getElementById("txt1").innerHTML;
     // escribimos en la base de datos, usamos el callback para redirigir la página
-    write(decisionActual, decisionTomada, function(){window.location.href = "#"})
+    write(decisionActual, decisionTomada, function(){window.location.href = url1})
 };
 
 document.getElementById("opc2").onclick = function(){
     let decisionTomada = document.getElementById("txt2").innerHTML;
-    write(decisionActual, decisionTomada, function(){window.location.href = "02.html"})
+    write(decisionActual, decisionTomada, function(){window.location.href = url2})
 };
